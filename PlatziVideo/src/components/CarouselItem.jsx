ec8fa615f25a50b263c9b3b46c0f56c94163ed69
RobM <robskdeveloper@gmail.com>
2021-01-24 14:39:42 -0600

    hasta leccion 28 - curso completo"

--- PlatziVideo/src/components/CarouselItem.jsx
+++ PlatziVideo/src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
@@ -21,6 +22,16 @@ const CarouselItem = ( { cover, title, year, contentRating, duration } ) => (
         </div>
     </div>
 );
+
+//Antes del export defaut, se validan los tipos de datos de los props que recibe este componente: cover, title, year, contentRating, y duration 
+//definimos los tipos de datos de este componente
+CarouselItem.propTypes = {  //OJO: se cambi de PropTypes a propTypes
+    cover: PropTypes.string,  //se define que este elemento llega como un string al campo de cover
+    title: PropTypes.string,
+    year: PropTypes.number,  //se define que este elemento llega como un numero al campo year
+    contentRating: PropTypes.string,
+    duration: PropTypes.number,  //de cambio de string a number
+}
 
 /*
 const CarouselItem = () => (
